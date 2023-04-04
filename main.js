3989d84d6958ab7d5467faf980bf10cfb2208fc0
Marco Marchi <115740827+MarquinCss@users.noreply.github.com>
2023-04-03 21:48:57 -0300

    Update main.js

--- main.js
+++ main.js
@@ -21,7 +21,9 @@ function off() {
       document.getElementById("btn").style.backgroundColor = "#000";
        document.getElementById("btnSom").style.backgroundColor = "#000";
         document.getElementById("btnSom2").style.backgroundColor = "#000";
-         document.getElementById("btn4").style.backgroundColor = "#000";
+         document.getElementById("btnSom3").style.backgroundColor = "#000";
+        document.getElementById("btnSom4").style.backgroundColor = "#000";
+
 }
 
 const botaoSom = document.querySelector('#btnSom');
@@ -36,5 +38,13 @@ const botaoSom2 = document.querySelector('#btnSom2');
 const som2 = new Audio('./mel.mp3');
 
 botaoSom2.addEventListener('click', function() {
+  som2.play();
+});
+
+
+const botaoSom3 = document.querySelector('#btnSom3');
+const som2 = new Audio('./telefone.mp3');
+
+botaoSom3.addEventListener('click', function() {
   som2.play();
 });
